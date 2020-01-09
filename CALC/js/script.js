8d1e20060088d6a2bd3da4b53c2d7aa90f9903e9
git-addSalah <salah.bouanba2@gmail.com>
2020-01-09 20:04:38 +0100

    JsCalc by GitSalah YouCode Safi

---
+++ CALC/js/script.js
@@ -0,0 +1,15 @@
+var num1;
+var num2;
+  function AdditionBy(num1,num2){       
+    document.getElementById("res").innerHTML = parseFloat(num1) + parseFloat(num2);
+  }
+  function substractionBy(num1,num2){
+    document.getElementById("res").innerHTML = parseFloat(num1) - parseFloat(num2);
+  }
+  function multiplyBy(num1,num2){
+    document.getElementById("res").innerHTML = parseFloat(num1) * parseFloat(num2);
+  }
+
+  function divideBy(num1,num2) {
+    document.getElementById("res").innerHTML = parseFloat(num1) / parseFloat(num2);
+  }
